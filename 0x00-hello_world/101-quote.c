f8851fe368f62f8cc21a00acab3668675cecd20e
SonicCalories <behateayoub@gmail.com>
2022-06-02 16:08:20 -0700

    0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
- * main - Entry point
+ * main - use to return the integer value 1
  *
- * Return: Always 0 (Success)
+ * Description: use to print to standard output in the main function
+ *
+ * Return: return the value 1
+ *
  */
+
 int main(void)
 {
-        fputs("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
-        return (0);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
