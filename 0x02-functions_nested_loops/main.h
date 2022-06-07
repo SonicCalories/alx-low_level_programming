2ca7fa518ec12c85c0a0afb7d975593d4a399bc8
SonicCalories <behateayoub@gmail.com>
2022-06-07 16:39:14 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+
+int _putchar(char c);
+
+void print_alphabet(void);
+void print_alphabet_x10(void);
+void printer(void);
+int print_sign(int n);
+int _isalpha(int c);
+int _abs(int);
+void print_to_98(int n);
+int add(int a, int b);
+int print_last_digit(int);
+int _islower(int c);
+#endif
