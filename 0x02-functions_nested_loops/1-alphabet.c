396fced27f44a51924d0b1027628e61e39b077f0
SonicCalories <behateayoub@gmail.com>
2022-06-07 16:46:39 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always (0)
+ */
+
+void print_alphabet(void)
+{
+	/**
+	* print_alphabet - prints all the alphabets
+	*
+	* Return: a character value
+	*/
+
+	int i = 97;
+
+	for (; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
