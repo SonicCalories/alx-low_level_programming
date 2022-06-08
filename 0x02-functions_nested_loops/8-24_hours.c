7ab683ef16a7826fdc45e6c9871921c2cc51702e
SonicCalories <behateayoub@gmail.com>
2022-06-07 18:47:24 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,24 +1,25 @@
 #include "main.h"
 
 /**
- * jack_bauer - Print every minute of the day of Jack Bauer
- * Description: Start from 00:00 to 23:59
+ * jack_bauer - prints some things
+ *
+ *
+ * Return: 0
  */
-
 void jack_bauer(void)
 {
-	int x, y;
+int i, j;
 
-	for (x = 0; x < 24; x++)
-	{
-		for (y = 0; y < 60; y++)
-		{
-			_putchar((x / 10) + '0');
-			_putchar((x % 10) + '0');
-			_putchar(':');
-			_putchar((y / 10) + '0');
-			_putchar((y % 10) + '0');
-			_putchar('\n');
-		}
-	}
+for (i = 0; i < 24; i++)
+{
+for (j = 0; j < 60; j++)
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+_putchar(':');
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
+_putchar('\n');
+}
+}
 }
