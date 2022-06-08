e701d9a4085b5d88266f5309434f70da594e1119
SonicCalories <behateayoub@gmail.com>
2022-06-07 17:06:10 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - get the absolute value of integer
+ * @n: number to be computed
+ *
+ * Return: computed value
+ */
+
+int _abs(int n)
+{
+	int computed = abs(n);
+
+	return (computed);
+}
